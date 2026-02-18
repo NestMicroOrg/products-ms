dce5b3557f667203afd030829733d97aa8633db8
JP19-03 <u202014511@upc.edu.pe>
2026-02-17 21:02:11 -0500

    feat: add @nestjs/microservices package and configure microservice transport in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      }
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +24,7 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(envs.PORT);
-  logger.log(`Application is running on: ${envs.PORT}`);
+  await app.listen();
+  logger.log(`Products Microservice is running on port ${envs.PORT}`);
 }
 bootstrap();
