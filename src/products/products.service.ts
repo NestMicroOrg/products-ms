22d280232e0f28cd04af231b6a3e6ab36472f973
JP19-03 <u202014511@upc.edu.pe>
2026-02-18 16:47:04 -0500

    feat: replace NotFoundException with RpcException for product retrieval errors

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma.service';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService {
@@ -52,7 +53,11 @@ export class ProductsService {
     })
 
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      //throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.NOT_FOUND
+      });
     }
 
     return product;
