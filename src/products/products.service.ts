cd34ea721a21dbc8b88c7274aa2c2745f5f16640
JP19-03 <u202014511@upc.edu.pe>
2026-02-17 20:53:46 -0500

    feat: enhance Product entity and service for availability checks and soft delete functionality

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -20,13 +20,20 @@ export class ProductsService {
 
     const { page = 1, limit = 10 } = paginationDto;
 
-    const totalPages = await this.prisma.product.count();
+    const totalPages = await this.prisma.product.count({
+      where: {
+        available: true
+      }
+    });
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.prisma.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: {
+          available: true
+        }
       }),
       meta: {
         totalPages,
@@ -36,15 +43,56 @@ export class ProductsService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.prisma.product.findFirst({
+      where: {
+        id,
+        available: true
+      }
+    })
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    const product = await this.prisma.product.update({
+      where: {
+        id
+      },
+      data: updateProductDto
+    })
+
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    // Hard delete - this actually deletes the record from the database
+    // await this.prisma.product.delete({
+    //   where: {
+    //     id
+    //   }
+    // });
+
+    // Soft delete - this just updates the available field to false
+    await this.prisma.product.update({
+      where: {
+        id
+      },
+      data: {
+        available: false
+      }
+    });
+
+    return {
+      message: `Product with id ${id} has been disabled`
+    }
   }
 }
