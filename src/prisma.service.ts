d7b037fb92838026d52c5df62c79edf1dbd6655c
JP19-03 <u202014511@upc.edu.pe>
2026-02-17 19:30:26 -0500

    fix: update PrismaClient import path to use generated client

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@nestjs/common';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
